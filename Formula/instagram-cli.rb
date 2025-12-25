9778e987941dd3ecb6b315fdc8dc6790cb875662
Jet Chiang <jetjiang.ez@gmail.com>
2025-12-25 12:20:31 -0500

    initial commit

---
+++ Formula/instagram-cli.rb
@@ -0,0 +1,18 @@
+class InstagramCli < Formula
+  desc "The unofficial CLI and terminal client for Instagram"
+  homepage "https://github.com/supreme-gg-gg/instagram-cli"
+  url "https://registry.npmjs.org/@i7m/instagram-cli/-/instagram-cli-1.3.1.tgz"
+  sha256 "6eca764c8ba271f1067f8343a5e09ee4a7df84b1fdc3332f2477cc3326552ed8"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/instagram-cli --version")
+  end
+end
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # homebrew-tap
+
+Homebrew Tap for [Instagram CLI](https://github.com/supreme-gg-gg/instagram-cli).
+
+```
+brew tap supreme-gg-gg/tap
+brew install instagram-cli
+```
+
+Homebrew will install node if it's not installed.
+We will consider submitting this to `homebrew-core` if community interest in high. We might consider releasing bottles (pre-built binaries) in the future.
+
+This repository is updated automatically by the Github Actions CI in `supreme-gg-gg/instagram-cli/.github/workflows/publish-ts.yaml`. Please do not open any issue or pull request here.
