cf2aadceda45fc28473274042db54279c049d328
Jet Chiang <jetjiang.ez@gmail.com>
2025-12-25 12:28:40 -0500

    fix audit error

--- Formula/instagram-cli.rb
+++ Formula/instagram-cli.rb
@@ -1,5 +1,5 @@
 class InstagramCli < Formula
-  desc "The unofficial CLI and terminal client for Instagram"
+  desc "Unofficial CLI and terminal client for Instagram"
   homepage "https://github.com/supreme-gg-gg/instagram-cli"
   url "https://registry.npmjs.org/@i7m/instagram-cli/-/instagram-cli-1.3.1.tgz"
   sha256 "6eca764c8ba271f1067f8343a5e09ee4a7df84b1fdc3332f2477cc3326552ed8"
