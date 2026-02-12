3a58646ffe93303e77860ce35ccabc760adaffdb
Jet Chiang <jetjiang.ez@gmail.com>
2026-02-12 13:12:11 -0500

    Sync latest 1.4.4 on npm

--- Formula/instagram-cli.rb
+++ Formula/instagram-cli.rb
@@ -1,8 +1,8 @@
 class InstagramCli < Formula
   desc "Unofficial CLI and terminal client for Instagram"
   homepage "https://github.com/supreme-gg-gg/instagram-cli"
-  url "https://registry.npmjs.org/@i7m/instagram-cli/-/instagram-cli-1.4.3.tgz"
-  sha256 "75235daa2ea74ccce6ee9fd8b6de1aeb08e405c04df571c163f7111ed9959741"
+  url "https://registry.npmjs.org/@i7m/instagram-cli/-/instagram-cli-1.4.4.tgz"
+  sha256 "2aa53d118d2dbc6649f3f4cc57ca2b65edc551736c27144117507b66ee754ebc"
   license "MIT"
 
   depends_on "node"
